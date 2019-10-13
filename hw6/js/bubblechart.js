03f62bbec396453bf3fd9e26e8e08b0adea161d3
LeeHome <lee.leavitt.u@gmail.com>
2019-10-13 14:13:48 -0600

    Significant progress

--- hw6/js/bubblechart.js
+++ hw6/js/bubblechart.js
@@ -6,9 +6,9 @@ class bubbleChart{
     constructor(govData){
         /**All Phrases the Govenors Use */
         this.govData = govData;
-        this.margin = { top: 20, right: 20, bottom: 60, left: 80 };
-        this.width = 900 - this.margin.left - this.margin.right;
-        this.height = 700 - this.margin.top - this.margin.bottom;
+        this.margin = { top: 20, right: 20, bottom: 60, left: 10 };
+        this.width = 1000 - this.margin.left - this.margin.right;
+        this.height = 1000 - this.margin.top - this.margin.bottom;
 
         console.log(govData)
         console.log(Math.max(...this.govData.map(d=>d.position) ) )
@@ -32,8 +32,12 @@ class bubbleChart{
             .text('Grouped by Topic')
             .append('input')
             .attr('type', 'checkbox')
-            .attr('data-toggle', 'toggle')
-            .attr('data-size', 'mini')
+            .attr('id', 'topicCheck')
+            // .attr('data-toggle', 'toggle')
+            // .attr('data-size', 'mini')
+            .on('click',d=>this.updateBubble())
+
+            //.on('click',this.updateBubble())
 
         d3.select('#switchContainer')
             .append('input')
@@ -43,6 +47,16 @@ class bubbleChart{
         //SVG to plot on
         d3.select('#bubbleChart')
             .append('div')
+            .attr('id', 'bChartWrap')
+
+        //tooltip div
+        d3.select('#bChartWrap')
+            .append('div')
+            .attr('class','tooltip')
+            .style('opacity',0)
+        
+        //Plot svg
+        d3.select('#bChartWrap')
             .append('svg')
             .attr('id', 'bubbleChart-svg')
             .attr('width', this.width)
@@ -61,21 +75,111 @@ class bubbleChart{
             .attr('id','x-axis')
             .attr('transform',`translate(${this.margin.left},${this.margin.top})`)
         
-        //x axis scale
+        //Add the xaxis to the plot
+        var xMax = Math.max(...this.govData.map(d=>d.position))
+        var xMin = Math.min(...this.govData.map(d=>d.position))
+        this.xScale = d3
+            .scaleLinear()
+            .domain([xMin,xMax])
+            .range([this.margin.left,950])
+            .nice();
+        
+        var xAxis = d3.axisTop(this.xScale)
+        xAxis.scale(this.xScale)
+
+        d3.select('#x-axis')
+            .classed('axis',true)
+            .call(xAxis)
+
+        //bubbleChart circel radius scale
+        var bubbleMax = Math.max(...this.govData.map(d=>d.total))
+        var bubbleMin = Math.min(...this.govData.map(d=>d.total))
+        this.bubbleScale = d3
+            .scaleLinear()
+            .domain([bubbleMin,bubbleMax])
+            .range([3,12])
+            .nice();
+        
+        //bubbleChart color values scale
+        const unique = (value, index, self) => {
+            return self.indexOf(value) === index
+        }
+        //unique categories
+        var uniqueCats = this.govData.map(d=>d.category).filter(unique)
+        var colors = ["#1b9e77","#d95f02","#7570b3","#e7298a","#66a61e","#e6ab02","#a6761d","#666666"]
+        this.bubbleColors = d3.scaleOrdinal()
+            .domain(uniqueCats)        
+            .range(colors)
+
         
 
+        this.bubbleChart('sourceX', 'sourceY')
 
-        var bubbleEnter = d3.select('#bubbleChart-svg').selectAll('circle')
-            //.select('.wrapper-group')
-            .data(this.govData)
-            .enter()
-            .append('circle')
-        
-            .attr('transform',`translate(${this.margin.left},100)`)
-            .attr('cx', d=>d.sourceX)
-            .attr('cy', d=> d.sourceY)
-            .attr('r', d=>d.total/10)
-            .attr('fill',null)
 
     }
+
+    bubbleChart(xValuePointer, yValuePointer){
+        
+        this.xVal = xValuePointer
+        this.yVal = yValuePointer
+
+        //Add the 0 line
+        
+        var bubbles = d3.select('#bubbleChart-svg').selectAll('circle').data(this.govData)
+            
+
+        var bubblesEnter = bubbles
+            .enter()
+            .append('circle')
+
+
+        bubbles.exit().remove()
+
+        bubbles = bubblesEnter.merge(bubbles)
+            .attr('transform',`translate(${this.margin.left},100)`)
+            .attr('cx', d=>d[this.xVal])
+            .attr('cy', d=> d[this.yVal])
+            .attr('r', d=>this.bubbleScale(d.total))
+            .attr('fill',d=>this.bubbleColors(d.category))
+            .attr('stroke','black')
+            .attr('stroke-width',1)
+            .on('mouseover', function(d){
+                var tooltip = d3.select('.tooltip')
+                tooltip.transition().duration(100).style('opacity',.9);
+                tooltip.html(
+                    d.phrase + '<br/>'
+                    + "R+" + Math.abs(d.percent_of_d_speeches - d.percent_of_r_speeches) + '<br/>'
+                    +"In "+ (d.total/50)+ "of speeches")
+                    .style('left', (d3.event.pageX)+'px')
+                    .style('top', (d3.event.pageY-28)+'px');
+            })
+            .on('mouseout', function(d){
+                var tooltip = d3.select('.tooltip')
+                tooltip.transition().duration(500).style('opacity')
+            })
+
+        
+        bubbles.transition().duration(500)
+
+        
+    }
+
+    updateBubble(){
+        if(document.getElementById('topicCheck').checked){
+            this.bubbleChart('moveX','moveY')
+        }else{
+            this.bubbleChart('sourceX','sourceY')
+        }
+    }
+
+    toolTipRender(d){
+        let text = '<h2>' + (d.phrase.charAt(0).toUpperCase() + d.phrase.substring(1)) + '</h2>'
+        // d.phrase + '<br/>'
+        // + "R+" + Math.abs(d.percent_of_d_speeches - d.percent_of_r_speeches) + '<br/>'
+        // +"In "+ (d.total/50)+ "of speeches")
+        // .style('left', (d3.event.pageX)+'px')
+        // .style('top', (d3.event.pageY-28)+'px');
+
+    }
+
 }
